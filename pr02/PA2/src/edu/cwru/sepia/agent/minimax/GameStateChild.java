a06bcd57792baa523016fd825d5cce8e8c17598e
Luc Bettaieb <luc.bettaieb@gmail.com>
2015-03-04 11:03:51 -0500

    added tree factor to help footmen realize trees are bad and impede them from attacking the enemy

--- pr02/PA2/src/edu/cwru/sepia/agent/minimax/GameStateChild.java
+++ pr02/PA2/src/edu/cwru/sepia/agent/minimax/GameStateChild.java
@@ -14,14 +14,12 @@ public class GameStateChild {
     //* This is the game state resulting from the specified set of actions
     public GameState state;
 
-    public GameStateChild(State.StateView state)
-    {
+    public GameStateChild(State.StateView state) {
         action = null;
         this.state = new GameState(state);
     }
 
-    public GameStateChild(Map<Integer, Action> action, GameState state)
-    {
+    public GameStateChild(Map<Integer, Action> action, GameState state) {
         this.action = action;
         this.state = state;
     }
