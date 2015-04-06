155063a9052cc0a7b3ec00840dd9226287edfc35
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-04-06 18:14:20 -0400

    Implemented MoveAction

--- pr03/src/edu/cwru/sepia/agent/planner/actions/MoveAction.java
+++ pr03/src/edu/cwru/sepia/agent/planner/actions/MoveAction.java
@@ -1,26 +1,43 @@
 package edu.cwru.sepia.agent.planner.actions;
 
 import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.GameState_old;
+import edu.cwru.sepia.environment.model.state.ResourceType;
 
 /**
  * Created by luc on 3/28/15.
  */
 public class MoveAction implements StripsAction {
+    private int peasantOfInterest = -1;
+    private ResourceType resourceType;
+    
+    public MoveAction(int peasantOfInterest, ResourceType resourceType){
+        this.peasantOfInterest = peasantOfInterest;
+        this.resourceType = resourceType;
+    }
+    
+    
     @Override
     public String getSentence() {
-        return null;
+        return "MOVE("+peasantOfInterest+","+resourceType.toString()+")";
     }
 
     @Override
     //The peasant must exist?  We're getting into some existential questions here.
     //I don't know
     public boolean preconditionsMet(GameState state) {
-        return false;
+        return true;//no preconditions
     }
 
     @Override
     //Make dat sucka move.
     public GameState apply(GameState state) {
-        return null;
+        GameState returnVar = new GameState(state,0d,this);
+        state.getPeasantTracker().get(peasantOfInterest).setBesideGold(false);
+        state.getPeasantTracker().get(peasantOfInterest).setBesideWood(false);
+        state.getPeasantTracker().get(peasantOfInterest).setBesideTH(false);
+        if(this.resourceType == ResourceType.WOOD) state.getPeasantTracker().get(peasantOfInterest).setBesideWood(true);
+        if(this.resourceType == ResourceType.GOLD) state.getPeasantTracker().get(peasantOfInterest).setBesideGold(true);
+        return returnVar;
     }
 }
